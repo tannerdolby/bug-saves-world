8ed30e32457e9429d6e6094f0c6983dc188acc14
Tanner <tannercdolby@gmail.com>
2021-11-11 12:48:59 -0800

    Add game objects to the scene

--- game.js
+++ game.js
@@ -3,24 +3,30 @@ let config = {
     type: Phaser.AUTO,
     // Set the dimensions for canvas element that
     // Phaser creates, this is the resolution
-    // game displays in
+    // game displays in: 0x0 - 800x600
     width: 800,
     height: 600, 
-    // handle physics
+    // use Arcade Physics system
     physics: {
         default: 'arcade',
-        acade: {
+        arcade: {
             gravity: {
-                y: 200
+                y: 300,
+                debug: false
             }
         }
     },
     // Default scene
+    // the scene itself doesn't have a fixed size,
+    // it extends infinitely in all directions
     scene: {
         preload: preload,
         create: create
     }
-
+    // The Camera system controls your view into
+    // the Scene and you can move/zoom the active
+    // camera as required, we can also create new
+    // cameras for multiple views into the Scene
 }
 
 // Create an instance of a Phaser.Game object 
@@ -28,23 +34,25 @@ let config = {
 var game = new Phaser.Game(config);
 
 // Load Assets 
-// Define a Scene function called "preload"
+// define a Scene function called "preload"
 // where assets are loaded
 function preload ()
 {
-    // Load Image Assets
+    // load Image Assets
     this.load.image("deepBlueSky", "assets/sky-deep-blue.png");
     this.load.image("lightBackground", "assets/yellow-bg.png");
     this.load.image("cloud", "assets/tacky-cloud.png");
     this.load.image("star", "assets/star.png");
+    this.load.image("ground", "assets/platform.png");
+    this.load.image("blackStar", "assets/black-stars.png");
+    this.load.image("goldStar", "assets/gold-star.png");
+    this.load.image("grassPlatform", "assets/grass-platform.png");
     
     // todo: make ladybug a spritesheet 
     // positions: (facing front,left,right,left running, right running)
     this.load.image("ladybug", "assets/ladybug.png");
 
     // todo: add special items/keys to be found e.g. stars/special item
-
-    // todo: load platforms
 
     // load a spritesheet
     this.load.spritesheet("dude", "assets/dude.png", {
@@ -56,22 +64,38 @@ function preload ()
 // Creating Game Objects
 // game objects are displayed in the order
 // you create them (in a stack context)
+// the Scenes display list is where all
+// game objects live
 function create ()
 {
-    // syntax: add.image(xCoord, yCoord, key)
+    // Create game objects and add them to the
+    // current Scenes display list
+    // syntax: this.add.image(xCoord, yCoord, key)
     this.add.image(400, 300, "deepBlueSky");
     this.add.image(400, 300, "lightBackground");
     this.add.image(150, 200, "cloud").setScale(.25);
     this.add.image(300, 100, "cloud").setScale(.3);
     this.add.image(580, 100, "cloud").setScale(.3);
-    this.add.image(100, 100, "star").setScale(.9);
-    this.add.image(450, 50, "star").setScale(.9);
-    this.add.image(650, 180, "star").setScale(.9);
-    this.add.image(400, 400, "ladybug").setScale(.25);
+    // Ladybug character (home screen position related)
+    this.add.image(400, 427, "ladybug").setScale(.265);
+    this.add.text(280, 200, "Bug Saves The World", { color: "#000", fontSize: "1.5rem" });
+    this.add.image(430, 100, "blackStar").setScale(.3);
+    this.add.image(200, 50, "blackStar").setScale(.3);
+    this.add.image(200, 50, "goldStar").setScale(.5);
+    this.add.image(100, 115, "blackStar").setScale(.3);
+    this.add.image(100, 115, "goldStar").setScale(.5);
+    this.add.image(430, 100, "goldStar").setScale(.5);
 
-    // todo: add physics, create staticGroup()
+    // stacking a gold star onto a black star looks cool
+    // gives it a more retro/game look
 
-    // handle jumping interation from platform to platform
+    // Platform game objects
+    // Create a group of Static Bodies (e.g. they don't move and are unaffected by collisions)
+    let platforms = this.physics.add.staticGroup();
+    platforms.create(10, 530, "grassPlatform").setScale(1, .7).refreshBody();
+    platforms.create(510, 530, "grassPlatform").setScale(1, .7).refreshBody();
+
+    // handle jumping motion from platform to platform
 }
 
 
